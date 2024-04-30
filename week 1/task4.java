0375c0f1dfd62847739fa5c31a639578ddee9f6b
Rikchhen <rikchhenlama3@gmail.com>
2024-04-30 10:22:18 +0545

    week1 task

---
+++ week 1/task4.java
@@ -0,0 +1,20 @@
+/* 4. WAP that prints the following pattern:
+
+              *
+
+              **
+
+              ***
+
+              **** */
+            
+
+              public class task4 {
+                    public static void main(String[] args) {
+                      System.out.println("*");
+                      System.out.println("**");
+                      System.out.println("***");
+                      System.out.println("****");
+                    }
+                  
+              }
---
+++ week 1/task5.java
@@ -0,0 +1,22 @@
+      /* 5. WAP prints the following pattern:
+
+            *********
+
+            *********
+
+            *********
+
+            ********* */
+           
+
+            public class task5 {
+                public static void main(String[] args) {
+                    System.out.println("*********");
+                    System.out.println("*********");
+                    System.out.println("*********");
+                    System.out.println("*********");
+                    
+                    
+                }
+                
+            }
